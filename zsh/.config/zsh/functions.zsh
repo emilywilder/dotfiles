6b2e397cac943b86b94e38728b8fa3e3c061fc20
Emily Lockhart <validitymaiden@gmail.com>
2025-04-23 13:33:41 -0400

    loop through functions loaded

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -5,12 +5,12 @@ case "${OSTYPE}" in
     ;;
 esac
 # check that these programs exist before loading their functions
-if which git >/dev/null; then
-    source $ZDOTDIR/functions.git.zsh
-fi
-if which python >/dev/null; then
-    source $ZDOTDIR/functions.python.zsh
-fi
+for _prog in git python; do
+    if which ${_prog} >/dev/null; then
+        source $ZDOTDIR/functions.${_prog}.zsh
+        echo "Locded functions.${_prog}"
+    fi
+done
 # check for machine specific functions
 case "$(hostname -s)" in
     athena)
