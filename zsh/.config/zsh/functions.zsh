84894f0dd200985ff81a9f238d4849f0ff2ed280
Emily Lockhart <validitymaiden@gmail.com>
2025-04-23 13:26:32 -0400

    make functions loaded platform and host specific. also check for programs before loading their functions

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,4 +1,19 @@
-source $ZDOTDIR/functions.athena.zsh
-source $ZDOTDIR/functions.git.zsh
-source $ZDOTDIR/functions.macos.zsh
-source $ZDOTDIR/functions.python.zsh
+# check for OS specific functions
+case "${OSTYPE}" in
+    darwin*)
+        source $ZDOTDIR/functions.macos.zsh
+    ;;
+esac
+# check that these programs exist before loading their functions
+if which git >/dev/null; then
+    source $ZDOTDIR/functions.git.zsh
+fi
+if which python >/dev/null; then
+    source $ZDOTDIR/functions.python.zsh
+fi
+# check for machine specific functions
+case "$(hostname -s)" in
+    athena)
+        source $ZDOTDIR/functions.athena.zsh
+    ;;
+esac
