de1bca5acb66d195d69e48773371df2511a27ba8
Emily Lockhart <validitymaiden@gmail.com>
2025-04-28 14:27:36 -0400

    move gittree into functions.git.zsh

--- zsh/.config/zsh/functions.git.zsh
+++ zsh/.config/zsh/functions.git.zsh
@@ -17,3 +17,12 @@ packsources() {
         return 1
     fi
 }
+
+gittree() {
+    git log \
+        --graph \
+        --full-history \
+        --all \
+        --color \
+        --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+}
