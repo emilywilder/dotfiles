e8c27536166cd531e25214bfd06f15c3140f19e4
Emily Lockhart <validitymaiden@gmail.com>
2025-04-28 11:42:21 -0400

    split gittree alias into multiline

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,4 +3,9 @@ alias lsd='ls -al | grep ^d'
 alias paths='echo ${PATH//:/\\n}'
 alias vi='vim'
 alias diff='diff -u --color'
-alias gittree='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
+alias gittree='git log \
+    --graph \
+    --full-history \
+    --all \
+    --color \
+    --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
