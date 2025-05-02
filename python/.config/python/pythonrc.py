7e01cf4459c4b9247ca3b7ad3af8b3806e8c3ea4
Emily Lockhart <validitymaiden@gmail.com>
2025-05-02 12:46:11 -0400

    remove commented code from pythonrc

--- python/.config/python/pythonrc.py
+++ python/.config/python/pythonrc.py
@@ -9,15 +9,6 @@ import atexit
 atexit.register(readline.write_history_file, histfile)
 del os, histfile
 
-
-#try:
-#    import readline
-#except ImportError:
-#    print "Module readline not available."
-#else:
-#    import rlcompleter
-#    readline.parse_and_bind("tab: complete")
-
 import rlcompleter
 if 'libedit' in readline.__doc__:
     readline.parse_and_bind("bind ^I rl_complete")
