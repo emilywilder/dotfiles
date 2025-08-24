e6fbca1f4f343f86d5e8f67f299052593a7bdc28
Emily Lockhart <validitymaiden@gmail.com>
2025-08-24 18:29:21 -0400

    zsh: - add support for eza

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,12 @@
-alias ls='ls --color'
-alias ll='ls -al'
-alias lsd='ll | grep ^d'
+if which eza >/dev/null; then
+    alias ls='eza --icons --color-scale --color-scale-mode=gradient --hyperlink'
+    alias ll='ls -l --git -M'
+    alias lsd='ll -D'
+else
+    alias ls='ls --color'
+    alias ll='ls -al'
+    alias lsd='ll | grep ^d'
+fi
 alias paths='echo ${PATH//:/\\n}'
 alias vi='vim'
 alias diff='diff -u --color'
