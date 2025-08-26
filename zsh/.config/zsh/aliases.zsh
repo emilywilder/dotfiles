fe5c4d1674af7d93b5163b9e30c9eaabe2a590ba
Emily Lockhart <validitymaiden@gmail.com>
2025-08-26 12:37:49 -0400

    zsh: - add lss alias for sorting by size - fix gnu ls usage to not show  hidden files (match eza usage) - fix gnu ls lsd alias to just use ls and not pipe to grep

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,10 +2,12 @@ if which eza >/dev/null; then
     alias ls='eza --icons --hyperlink'
     alias ll='ls -l --git -M'
     alias lsd='ll -D'
+    alias lss='ll --sort=size --color-scale'
 else
     alias ls='ls --color'
-    alias ll='ls -al'
-    alias lsd='ll | grep ^d'
+    alias ll='ls -lh'
+    alias lsd='ll -d */'
+    alias lss='ll -Sr'
 fi
 alias paths='echo ${PATH//:/\\n}'
 alias vi='vim'
