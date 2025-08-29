ca9c95bd97daff550fe4fe1744b19c7a71f69766
Emily Lockhart <validitymaiden@gmail.com>
2025-08-29 09:11:44 -0400

    zsh: - fzf: show eza in a fzf-preview box

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -2,5 +2,5 @@
 if which fzf >/dev/null; then
     source $ZDOTDIR/plugins/fzf-tab/fzf-tab.plugin.zsh
     # preview directory's content with eza when completing cd
-    zstyle ':fzf-tab:complete:cd:*' fzf 'eza --icons=always --width=1 --color=always $realpath'
+    zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --icons=always --width=1 --color=always $realpath'
 fi
