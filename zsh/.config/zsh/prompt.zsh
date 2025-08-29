b7ebb8b64d73280e9231f9bfb81876ee0829bcd8
Emily Lockhart <validitymaiden@gmail.com>
2025-08-29 09:17:04 -0400

    zsh: - add comment re ternary expression

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -10,5 +10,9 @@ precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 
 RPROMPT='[%?]'
+
+# uses a ternary expression as defined in
+# https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html#Conditional-Substrings-in-Prompts
+
 PROMPT='%B%F{green}%~%f %B%F{cyan}${vcs_info_msg_0_}%f
 %(2L.(${SHLVL}) .)%F{white}%n@%m %# %f%b'
