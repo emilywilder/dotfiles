6bcbc5c695476623d7f134758d93f61d632e4f06
Emily Lockhart <validitymaiden@gmail.com>
2025-10-10 14:07:11 -0400

    refactor(zsh): use autoload for nix functions
    Rebasing functions_rework
    
    added nix-get-flake-outpaths to original commit
    
    # Conflicts:
    #	zsh/.config/zsh/functions.nix.zsh

--- zsh/.config/zsh/functions.nix.zsh
+++ zsh/.config/zsh/functions.nix.zsh
@@ -1,32 +1,11 @@
-nix-deps() {
-    if [ $# -eq 1 ]; then
-        nix derivation show $1 |
-            jq -r '.[].inputDrvs | keys.[] |
-                match(".*?-(.*)\\.drv"; "g").captures[0].string'
-    else
-        echo "usage: ${funcstack[-1]} <packageName>" >&2
-        return 1
-    fi
-}
+#!/bin/zsh
 
-nix-get-pname() {
-    if [ $# -eq 1 ]; then
-        nix derivation show $1 |
-          jq '.[].env.pname'
-    else
-        echo "usage: ${funcstack[-1]} <installable>" >&2
-        return 1
-    fi
-}
+ZSH_FUNCTIONS_BASEDIR=$0:h/functions
+fpath+=($ZSH_FUNCTIONS_BASEDIR)
 
-nix-get-flake-outpaths() {
-    if [ $# -eq 1 ]; then
-        local _dir
-        if _dir="$(nix build "${1}" --print-out-paths --no-link)"; then
-            find "$_dir"
-        fi
-    else
-        echo "usage: ${funcstack[-1]} <nixpkgs#name>" >&2
-        return 1
-    fi
-}
+FUNCTIONS=(nix-deps nix-get-pname nix-get-flake-outpaths)
+
+# Load functions
+for _f in ${ZSH_FUNCTIONS_BASEDIR}/${FUNCTIONS}; do
+    autoload ${_f}
+done
