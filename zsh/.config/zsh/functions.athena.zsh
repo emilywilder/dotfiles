f68c1f3e9ac26ea7ff1a64ed25e353eea68eca28
Emily Lockhart <validitymaiden@gmail.com>
2025-10-10 13:44:43 -0400

    refactor(zsh): use autoload for athena functions
    Utilize zsh's autoload feature for custom functions, starting with functions specific to the "athena" scope.
    
    Use a functions directory to store each function rather than a sourced .zsh file.

--- zsh/.config/zsh/functions.athena.zsh
+++ zsh/.config/zsh/functions.athena.zsh
@@ -1,19 +1,11 @@
-zebesmount() {
-    if [ -n "$1" -a -n "$2" ]; then
-        _remotepath="$1"
-        _volname="$2"
-        _mountpoint="/Volumes/${_volname}"
-        _remotemountpoint="${_host}:${_remotepath}"
-        _host="zebes"
-        if [ -d "$_mountpoint" ]; then
-            echo "Removing $_mountpoint..."
-            rmdir "$_mountpoint"
-        fi
-        if sudo install -d -o $USER "$_mountpoint"; then
-            sshfs "$_remotemountpoint" "$_mountpoint" -o volname="$_volname"
-        fi
-    else
-        echo "usage: $FUNCNAME <mountpoint> <volname>" >&2
-        return 1
-    fi
-}
+#!/bin/zsh
+
+ZSH_FUNCTIONS_BASEDIR=$0:h/functions
+fpath+=($ZSH_FUNCTIONS_BASEDIR)
+
+FUNCTIONS=(zebesmount)
+
+# Load functions
+for _f in ${ZSH_FUNCTIONS_BASEDIR}/${FUNCTIONS}; do
+    autoload ${_f}
+done
