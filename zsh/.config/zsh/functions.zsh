5ba8a9776eb5852d2ef19aa64266b67c1ce72048
Emily Lockhart <validitymaiden@gmail.com>
2025-10-10 21:34:43 -0400

    refactor(zsh): function loading scope
    Move function autoloading into an anonymous function to allow for local variables
    
    Set scope and mutability of function autoloading specific variables

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,46 +1,50 @@
 ZSH_FUNCTIONS_BASEDIR=$0:h/functions
 fpath+=($ZSH_FUNCTIONS_BASEDIR)
 
-supported_programs=(
-    git
-    nix
-    python
-)
+() {
+    readonly supported_programs=(
+        git
+        nix
+        python
+    )
 
-_functions_athena=(zebesmount)
-_functions_git=(packsources gittree)
-_functions_macos=(attachiso dequarantine)
-_functions_nix=(nix-deps nix-get-pname nix-get-flake-outpaths)
-_functions_python=(python_ensurepip pip_updateall)
+    readonly _functions_athena=(zebesmount)
+    readonly _functions_git=(packsources gittree)
+    readonly _functions_macos=(attachiso dequarantine)
+    readonly _functions_nix=(nix-deps nix-get-pname nix-get-flake-outpaths)
+    readonly _functions_python=(python_ensurepip pip_updateall)
 
-_functions_to_load=()
+    local _functions_to_load=()
 
-# check for OS specific functions
-case "${OSTYPE}" in
-    darwin*)
-        _functions_to_load+=(${(A)_functions_macos})
-    ;;
-esac
+    # check for OS specific functions
+    case "${OSTYPE}" in
+        darwin*)
+            _functions_to_load+=(${(A)_functions_macos})
+        ;;
+    esac
 
-# check that these programs exist before loading their functions
-for _prog in $supported_programs; do
-    if which ${_prog} >/dev/null; then
-        # construct variable expansion necessary to reference the array
-        # specific to the iterated program
-        local _varname="\${(A)_functions_$_prog}"
-        # use eval to dereference the constructed variable
-        _functions_to_load+=($(eval echo $_varname))
-    fi
-done
+    # check that these programs exist before loading their functions
+    local _prog
+    for _prog in $supported_programs; do
+        if which ${_prog} >/dev/null; then
+            # construct variable expansion necessary to reference the array
+            # specific to the iterated program
+            local _varname="\${(A)_functions_$_prog}"
+            # use eval to dereference the constructed variable
+            _functions_to_load+=($(eval echo $_varname))
+        fi
+    done
 
-# check for machine specific functions
-case "$(hostname -s)" in
-    athena)
-        _functions_to_load+=(${(A)_functions_athena})
-    ;;
-esac
+    # check for machine specific functions
+    case "$(hostname -s)" in
+        athena)
+            _functions_to_load+=(${(A)_functions_athena})
+        ;;
+    esac
 
-# Load functions
-for _f in ${ZSH_FUNCTIONS_BASEDIR}/${_functions_to_load}; do
-    autoload ${_f}
-done
+    # Load functions
+    local _f
+    for _f in ${ZSH_FUNCTIONS_BASEDIR}/${_functions_to_load}; do
+        autoload ${_f}
+    done
+}
