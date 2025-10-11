9980822786180bea9ee620335eca92b66c400f9b
Emily Lockhart <validitymaiden@gmail.com>
2025-10-10 20:39:28 -0400

    refactor(zsh): consolidate function scripts into functions.zsh
    Replace functions.X.zsh files with domain specific arrays in functions.zsh.
    Each conditional path now adds its domain specific array of functions to a single array _functions_to_load.
    Finally, the contents of _functions_to_load are set to autoload.

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,18 +1,40 @@
+ZSH_FUNCTIONS_BASEDIR=$0:h/functions
+fpath+=($ZSH_FUNCTIONS_BASEDIR)
+
+_functions_athena=(zebesmount)
+_functions_git=(packsources gittree)
+_functions_macos=(attachiso dequarantine)
+_functions_nix=(nix-deps nix-get-pname nix-get-flake-outpaths)
+_functions_python=(python_ensurepip pip_updateall)
+
+_functions_to_load=()
+
 # check for OS specific functions
 case "${OSTYPE}" in
     darwin*)
-        source $ZDOTDIR/functions.macos.zsh
+        _functions_to_load+=(${(A)_functions_macos})
     ;;
 esac
+
 # check that these programs exist before loading their functions
 for _prog in git python nix; do
     if which ${_prog} >/dev/null; then
-        source $ZDOTDIR/functions.${_prog}.zsh
+        # construct variable expansion necessary to reference the array
+        # specific to the iterated program
+        local _varname="\${(A)_functions_$_prog}"
+        # use eval to dereference the constructed variable
+        _functions_to_load+=($(eval echo $_varname))
     fi
 done
+
 # check for machine specific functions
 case "$(hostname -s)" in
     athena)
-        source $ZDOTDIR/functions.athena.zsh
+        _functions_to_load+=(${(A)_functions_athena})
     ;;
 esac
+
+# Load functions
+for _f in ${ZSH_FUNCTIONS_BASEDIR}/${_functions_to_load}; do
+    autoload ${_f}
+done
