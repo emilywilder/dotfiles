86d9956147866a8e430e686788350c8fb8690c94
Emily Lockhart <validitymaiden@gmail.com>
2025-11-07 23:25:47 -0500

    build(zsh)!: remove copied fzf-tab plugin
    - Removed fzf-tab plugin files that were manually copied into place
    - Will be refactoring this to use a submodule
    - Removed sourcing of fzf-tab.plugin.zsh
    - Removed zstyle for fzf-tab

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -1,8 +1,8 @@
 # https://github.com/Aloxaf/fzf-tab
-if which fzf >/dev/null; then
-    source $ZDOTDIR/plugins/fzf-tab/fzf-tab.plugin.zsh
-    # preview directory's content with eza when completing cd
-    zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --icons=always --width=1 --color=always $realpath'
-fi
+#if which fzf >/dev/null; then
+#    source $ZDOTDIR/plugins/fzf-tab/fzf-tab.plugin.zsh
+#    # preview directory's content with eza when completing cd
+#    zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --icons=always --width=1 --color=always $realpath'
+#fi
 
 source $ZDOTDIR/plugins/zsh-functional/functional.plugin.zsh
