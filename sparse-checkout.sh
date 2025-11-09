3757fb1e5df6d8a4536e79742f2cd1dd9bd24d6e
Emily Lockhart <validitymaiden@gmail.com>
2025-11-08 19:10:45 -0500

    build(git): add sparse-checkout.sh
    - Added the script sparse-checkout.sh
    - This will be called by `git submodule foreach` to apply the supermodule's .gitmodules settings to each submodule configured.

---
+++ sparse-checkout.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+TRAPZERR() {
+    if [[ "$pipestatus" -gt 0 ]]; then
+        echo "Error encountered. Exiting."
+        exit 1
+    fi
+}
+
+config_file="$toplevel/.gitmodules"
+config_key="submodule.$name.sparseCheckout"
+sparse_file_src="$toplevel/$name.sparse-checkout"
+sparse_file_dest="$toplevel/$GIT_DIR/modules/$name/info/sparse-checkout"
+
+if git config --file $config_file --get $config_key; then
+    echo "Sparse checkout enabled for submodule $name"
+    git sparse-checkout set
+    install -Cv $sparse_file_src $sparse_file_dest
+else
+    echo "Sparse checkout not enabled for submodule $name"
+    git sparse-checkout disable
+    [[ -e $sparse_file_dest ]] && (rm -v $sparse_file_dest) || true
+fi
+git checkout HEAD
+git status | (grep sparse || echo "Not a sparse checkout")
