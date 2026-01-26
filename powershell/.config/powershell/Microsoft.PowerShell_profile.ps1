c8622d8a0c2f320ed78ddb5cb75686348a06b384
Emily Lockhart <validitymaiden@gmail.com>
2026-01-26 14:34:56 -0500

    feat(powershell): use zsh style tab completion

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,12 @@
 # Exit a shell with ^D
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
+# Set tab completion to emulate ZSH
+# Source: https://gist.github.com/MeganaD/fe3894b39697da767b8b19b3e0bc047b
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
 # Provide variables for WSL home directories
 Set-Variable -Name "nixos_home" -Value "\\wsl.localhost\NixOS\home\$env:username"
