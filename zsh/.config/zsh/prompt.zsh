8b7a546ce544ff92b67a4538de15aebf4245e0ab
Emily Lockhart <validitymaiden@gmail.com>
2026-03-02 17:57:38 -0500

    refactor(zsh): move prompt shell status to variable

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -12,9 +12,10 @@ precmd_functions+=( precmd_vcs_info )
 # uses a ternary expression as defined in
 # https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html#Conditional-Substrings-in-Prompts
 
+_shell_level_status='%(2L.(%L) .)'
 _prev_cmd_status='%(?.️%F{green}.%F{red})⏺%F{white}'
 
 RPROMPT='[%?]'
 
 PROMPT='%B%F{green}%~%f %B%F{cyan}${vcs_info_msg_0_}%f
-%(2L.(%L) .)${_prev_cmd_status} %F{white}%n@%m %# %f%b'
+${_shell_level_status}${_prev_cmd_status} %F{white}%n@%m %# %f%b'
