emilywilder/dotfiles

commit 4ddc6574dee6acc0031541dc433d0fb2b116d7c0

refactor(zsh): ♻️ add unix generic os support to zsh functions.zsh

zsh/.config/zsh/functions.zsh
@@ -10,10 +10,14 @@ fpath+=($ZSH_FUNCTIONS_BASEDIR)
         python
     )
 
+    # hosts
     readonly functions_athena=(zebesmount bootstrap-nix)
+    # systems
+    readonly functions_unix=(glyph2codepoint)
+    readonly functions_linux=()
+    readonly functions_macos=(attachiso dequarantine plist2jq open)
+    # programs
     readonly functions_git=(packsources gittree jetbrains-get-gitignore)
-    readonly functions_macos=(attachiso dequarantine plist2jq open glyph2codepoint)
-    readonly functions_linux=(glyph2codepoint)
     readonly functions_nix=(nix-deps nix-get-pname nix-get-flake-outpaths nix-search-names)
     readonly functions_python=(python_ensurepip pip_updateall)
 
@@ -21,8 +25,9 @@ fpath+=($ZSH_FUNCTIONS_BASEDIR)
 
     # check for OS specific functions
     case "${OSTYPE}" in
-        darwin*)    functions_to_load+=(${(A)functions_macos}) ;;
-        linux*)     functions_to_load+=(${(A)functions_linux}) ;;
+        linux*|darwin*)    functions_to_load+=(${(A)functions_unix}) ;;
+        linux*)            functions_to_load+=(${(A)functions_linux}) ;;
+        darwin*)           functions_to_load+=(${(A)functions_macos}) ;;
     esac
 
     # check that these programs exist before loading their functions

commit cc122d3cb711102df3892355fa8515a4ce5baaa9

fix(zsh): 🐛 fix OS specific functions not loading
- Fixed issue introduced in 4ddc6574 where the linux* and darwin* case conditions were never evaluated due to the top-most condition always being true for these scenarios.
- Moved generic OS test into its own case statement.

zsh/.config/zsh/functions.zsh
@@ -25,9 +25,13 @@ fpath+=($ZSH_FUNCTIONS_BASEDIR)
 
     local functions_to_load=()
 
-    # check for OS specific functions
+    # check for OS generic functions
     case "${OSTYPE}" in
         linux*|darwin*)    functions_to_load+=(${(A)functions_unix}) ;;
+    esac
+
+    # check for OS specific functions
+    case "${OSTYPE}" in
         linux*)            functions_to_load+=(${(A)functions_linux}) ;;
         darwin*)           functions_to_load+=(${(A)functions_macos}) ;;
     esac