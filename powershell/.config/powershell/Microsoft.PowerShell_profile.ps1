3e6b6bf761c7875844a875cfaa727a7ca744f316
Emily Lockhart <validitymaiden@gmail.com>
2026-04-08 13:38:37 -0400

    refactor(windows): ️🧑‍💻 improve PS profile
    - Consolidated HistorySearchBackward, HistorySearchForward, and ViExit by setting EditMode to Eamcs.
    - Added ^LeftArrow and ^RightArrow for moving between words.
    - Added comment blocks

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,20 @@
-# Exit a shell with ^D
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+# Readline
 
-# Set tab completion to emulate ZSH
-# Source: https://gist.github.com/MeganaD/fe3894b39697da767b8b19b3e0bc047b
+## Start with emacs readline (this includes ^D exit)
+Set-PSReadLineOption -EditMode Emacs
+
+## Use arrow keys to move between words
+Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
+
+## Set tab completion to emulate ZSH
+## Source: https://gist.github.com/MeganaD/fe3894b39697da767b8b19b3e0bc047b
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+## Put cursor at end of line for history selection
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 
-# Provide variables for WSL home directories
+# Variables
+
+## Provide variables for WSL home directories
 Set-Variable -Name "nixos_home" -Value "\\wsl.localhost\NixOS\home\$env:username"
