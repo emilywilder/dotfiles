8a9125e4790b2f2afec9322303ef14a72e782e44
Emily Lockhart <validitymaiden@gmail.com>
2026-04-16 13:24:12 -0400

    refactor(powershell): ♻️ Improve profile load time
    - Put profile commands inside a check for running inside an interactive shell.
    - Wrapped profile sections in functions.

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,45 @@
-# Readline
-
-## Start with emacs readline (this includes ^D exit)
-Set-PSReadLineOption -EditMode Emacs
-
-## Use arrow keys to move between words
-Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
-Set-PSReadlineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
-
-## Set tab completion to emulate ZSH
-## Source: https://gist.github.com/MeganaD/fe3894b39697da767b8b19b3e0bc047b
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-## Put cursor at end of line for history selection
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-
-# Variables
-
-## Provide variables for WSL home directories
-Set-Variable -Name "nixos_home" -Value "\\wsl.localhost\NixOS\home\$env:username"
-
-# Hooks
-
-## atuin
-if ( Get-Command atuin )
+function Set-Readline
 {
-    atuin init powershell | Out-String | Invoke-Expression
+    ## Start with emacs readline (this includes ^D exit)
+    Set-PSReadLineOption -EditMode Emacs
+
+    ## Use arrow keys to move between words
+    Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow -Function BackwardWord
+    Set-PSReadlineKeyHandler -Key Ctrl+RightArrow -Function ForwardWord
+
+    ## Set tab completion to emulate ZSH
+    ## Source: https://gist.github.com/MeganaD/fe3894b39697da767b8b19b3e0bc047b
+    Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+    ## Put cursor at end of line for history selection
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 }
 
-## fzf
-if ( Get-InstalledModule -Name PSFzf )
+function Set-Variables
 {
-    Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-    Set-PsFzfOption -TabCompletionPreviewWindow 'right|down|hidden'
+    ## Provide variables for WSL home directories
+    Set-Variable -Name "nixos_home" -Value "\\wsl.localhost\NixOS\home\$env:username"
+}
+
+function Set-Hooks
+{
+    ## atuin
+    if (Get-Command atuin)
+    {
+        atuin init powershell | Out-String | Invoke-Expression
+    }
+
+    ## fzf
+    if (Get-InstalledModule PSFzf)
+    {
+        Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+        Set-PsFzfOption -TabCompletionPreviewWindow 'right|down|hidden'
+    }
+}
+
+if ( [Environment]::UserInteractive )
+{
+    Set-Readline
+    Set-Variables
+    Set-Hooks
 }
