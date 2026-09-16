9f618874a83d525692a323c6ecf63e8798fae16d
Emily Lockhart <validitymaiden@gmail.com>
2026-09-15 21:40:32 -0400

    feat(staux): ✨ support package entries using Bourne shell globbing patterns
    - Added function Resolve-BournePath to translate the Bourne shell style wildcards '*' and '*/' into their intended powershell entries.
        - This is to allow staux to maintain argument compatibility for the commonly used pattern '*/' for stowing all packages.

--- staux.ps1
+++ staux.ps1
@@ -16,11 +16,71 @@ Write-Debug "Restow: ${Restow}"
 Write-Debug "Delete: ${Delete}"
 Write-Debug "Packages: ${Packages}"
 
+function Resolve-BournePath {
+    <#
+    .SYNOPSIS
+        Resolve Bourne style paths and wildcards to their Powershell equivalents.
+    .DESCRIPTION
+        Given an array of paths, treat wildcard entries as Bourne shell compatible globs.
+        The supported wildcard entries are translated as:
+            '*'     All non-hidden files in the current directory
+            '*/'    All non-hidden directories in the current directory
+
+        Wildcard entries can be part of an array of other entries and will expand the output array.
+        For example, @( 'file1', '*' ) could map to @( 'file1', 'file1', 'file2', 'file3' ) given the appropriate
+        directory structure.
+
+        Paths can be given through the named parameter `Path` or supplied through the pipeline.
+    .EXAMPLE
+        Given the current working directory listing of
+
+            Mode  Name
+            ----  ----
+            d---- dir1
+            d---- dir2
+            -a--- .hiddenFile
+            -a--- file1
+            -a--- file2
+
+        `Resolve-BournePath -Path '*'` would output `@( 'dir1', 'dir2', 'file1', 'file2' )` while
+        `Resolve-BournePath -Path '*/'` would output `@( 'dir1', 'dir2' )`
+        `@( '*/' ) | Resolve-BournePath` also functions the same.
+
+        `Resolve-BournePath -Path 'file1', '*/', 'dir2'` would output @( 'file1', 'dir1', 'dir2', 'dir2' )
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'Path')]
+    param (
+    # Path or wildcard to process
+        [Parameter(Position = 0,
+                ParameterSetName = "Path",
+                Mandatory = $true,
+                ValueFromPipeline = $true,
+                ValueFromPipelineByPropertyName = $true)]
+        [String[]]
+        $Path
+    )
+
+    process {
+        $Path | ForEach-Object {
+            # conform to Bourne-style globbing for * and */
+            # powershell doesn't consider filenames beginning with '.' to be hidden,
+            # nor does it translate */ as directories
+            if ($_ -eq '*/') {
+                Get-ChildItem -Path . -Directory -Exclude .* -Name
+            } elseif ($_ -eq '*') {
+                Get-ChildItem -Path . -Exclude .* -Name
+            } else {
+                $_
+            }
+        }
+    }
+}
+
 switch ($PSCmdlet.ParameterSetName)
 {
     'Restow' {
         Write-Verbose "Using action Restow"
-        throw [System.NotImplementedException] "Not implemented."
+        $Packages | Resolve-BournePath
     }
     'Delete' {
         Write-Verbose "Using action Delete"
